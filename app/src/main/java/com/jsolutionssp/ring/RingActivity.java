e6738397e162b641dad39b003db7df6ac350a53b
Joaquin Fernandez <joaquin@asperasoft.com>
2017-05-04 07:56:36 +0200

    Initial commit

---
+++ app/src/main/java/com/jsolutionssp/ring/RingActivity.java
@@ -0,0 +1,219 @@
+
+package com.jsolutionssp.ring;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.appwidget.AppWidgetManager;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.jsolutionssp.ring.R;
+import com.jsolutionssp.ring.widget.WidgetProvider;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class RingActivity extends Activity {
+
+	private SharedPreferences settings;
+
+	private boolean notFirstTime = false;
+
+	private AdView mAdView;
+
+	public static String PREFS_NAME = "com.jsolutionssp.ring";
+
+	private static String ANALYTICS_ID = "UA-32596762-1";
+
+	public static final int five_week_calendar = 35;
+
+	public static final int six_week_calendar = 42;
+
+	public static final int months = 11; //From 0 to 11
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//Remove title bar
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);//Theme settings
+		settings = getSharedPreferences(RingActivity.PREFS_NAME, 0);
+		Editor editor = settings.edit();
+		editor.putInt("ringTakingDays", 21);
+		editor.putInt("ringRestDays", 7);
+		editor.commit();
+		String[] themeNames = getResources().getStringArray(R.array.theme_names);
+		ChangeTheme.iniTheme(settings, themeNames);
+		setContentView(R.layout.main);
+		mAdView = (AdView) findViewById(R.id.adView);
+		AdRequest adRequest = new AdRequest.Builder().build();
+		mAdView.loadAd(adRequest);
+		setTabBackground();
+		initialize();
+		widgetUpdate();
+	}
+
+	private void widgetUpdate() {
+		GregorianCalendar date = new GregorianCalendar();
+		//Update widget info
+		WidgetProvider.infoUpdate(RingLogics.isRingDay(settings, 
+				date.get(Calendar.DAY_OF_YEAR), date.get(Calendar.YEAR)));
+		WidgetProvider.updateWidgetContent(this,
+				AppWidgetManager.getInstance(this));
+
+	}
+
+	public void setTabBackground() {
+		if (notFirstTime) {
+			RelativeLayout tab = (RelativeLayout) findViewById(R.id.tab1);
+			tab.setBackgroundResource(ChangeTheme.background);
+		}
+		notFirstTime = true;
+	}
+
+	private void initialize() {
+		if (settings.getInt("startCycleDayofYear", -1) == -1) {
+			Toast t = Toast.makeText(getApplicationContext(), R.string.set_taking_ringday_toast, Toast.LENGTH_LONG);
+			//Twice so it stays the double of time
+			t.show();
+			t.show();
+		}
+		createTab();
+	}
+
+	private void createTab() {
+		Resources res = getResources();
+
+		TabHost tabs=(TabHost)findViewById(android.R.id.tabhost);
+		tabs.setup();
+
+		TabHost.TabSpec spec=tabs.newTabSpec("Calendary");
+		spec.setContent(R.id.tab1);
+		spec.setIndicator("",
+				res.getDrawable(android.R.drawable.ic_menu_month));
+		tabs.addTab(spec);
+
+		spec=tabs.newTabSpec("Preferences");
+		spec.setContent(R.id.tab2);
+		spec.setIndicator("",
+				res.getDrawable(android.R.drawable.ic_menu_manage));
+		tabs.addTab(spec);
+
+		tabs.setCurrentTab(0);
+	}
+
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		finish();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.MnuOpt1:
+			PackageManager manager = getApplicationContext().getPackageManager();
+			PackageInfo info = null;
+			try {
+				info = manager.getPackageInfo(getPackageName(), 0);
+			} catch (NameNotFoundException e) {
+			}
+			String version;
+			if (info == null) {
+				version = null;
+			}
+			else
+				version = info.versionName;
+			
+			String myVersion = getResources().getString(R.string.app_version);
+			//get the TextView from the custom_toast layout
+			final Dialog dialog = new Dialog(this,R.style.NoTitleDialog);
+			dialog.setContentView(R.layout.about_dialog);
+
+			TextView text = (TextView) dialog.findViewById(R.id.about_dialog_version);
+			text.setText(myVersion + " " + version);
+
+			Button button = (Button) dialog.findViewById(R.id.about_dialog_rate);
+			button.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					try {
+						Intent intent = new Intent(Intent.ACTION_VIEW); 
+						intent.setData(Uri.parse("market://details?id=com.jsolutionssp.ring")); 
+						startActivity(intent);
+					} catch (ActivityNotFoundException e) {
+						Intent intent = new Intent(Intent.ACTION_VIEW); 
+						intent.setData(Uri.parse("http://market.android.com/details?id=com.jsolutionssp.ring")); 
+						startActivity(intent);
+					}
+					dialog.dismiss();
+					finish();
+				}
+			});
+			Button button2 = (Button) dialog.findViewById(R.id.about_dialog_ok);
+			button2.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					dialog.dismiss();
+				}
+			});
+			dialog.show();
+			return true;
+		case R.id.MnuOpt2:
+			final Dialog intructionsDialog = new Dialog(RingActivity.this, R.style.NoTitleDialog);
+			intructionsDialog.setContentView(R.layout.instructions_dialog);
+			TextView dialogText = (TextView) intructionsDialog.findViewById(R.id.info_dialog_text);
+			dialogText.setText(R.string.instructions);
+			Button button1 = (Button) intructionsDialog.findViewById(R.id.info_dialog_button);
+			button1.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					intructionsDialog.dismiss();
+				}
+			});
+			intructionsDialog.show();
+			return true;
+		case R.id.MnuOpt3:
+			finish();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+}
